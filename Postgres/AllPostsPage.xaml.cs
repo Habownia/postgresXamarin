0296bc5649b322e2fa6975eefff2299a06d8e65f
Habownia <hubertkrz7@gmail.com>
2024-04-01 15:23:03 +0200

    Navigation

--- Postgres/AllPostsPage.xaml.cs
+++ Postgres/AllPostsPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Postgres.Lib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,11 +11,23 @@ using Xamarin.Forms.Xaml;
 namespace Postgres
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class AllPostsPage : ContentView
+    public partial class AllPostsPage : ContentPage
     {
+        private readonly HelperDB helper = new HelperDB();
+
         public AllPostsPage()
         {
             InitializeComponent();
+
+            //ExecInsert(14, "test", "b");
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            // shows data from db in a view
+            postListView.ItemsSource = await helper.ShowAllPosts();
         }
     }
 }
--- Postgres/MainPage.xaml.cs
+++ Postgres/MainPage.xaml.cs
@@ -10,90 +10,21 @@ namespace Postgres
 {
     public partial class MainPage : ContentPage
     {
-        private readonly HelperDB helper = new HelperDB();
 
         public MainPage()
         {
             InitializeComponent();
-
-            //ExecInsert(14, "test", "b");
         }
 
-        protected override async void OnAppearing()
+        private async void GoToAllPosts(object sender, EventArgs e)
         {
-            base.OnAppearing();
-
-            // shows data from db in a view
-            postListView.ItemsSource = await helper.ShowAllPosts();
+            await Navigation.PushAsync(new AllPostsPage());
         }
 
-
-
-        //public static async Task<NpgsqlConnection> InitDB()
-        //{
-        //    // nie powinno się dawać hasła do db w kodzie, ale w takim razie jak Pan miałby to odpalić? ;)
-        //    string connStr = $"Host={GetHost()};Username=postgres;Password=postgres;Database=blog";
-        //    var conn = new NpgsqlConnection(connStr);
-
-        //    try { await conn.OpenAsync(); }
-        //    catch (Exception ex) { Console.WriteLine($"Connection to DB failed: {ex.Message}"); }
-
-        //    return conn;
-        //}
-
-
-
-        //public async void ExecInsert(int id, string name, string desc)
-        //{
-        //    // https://www.npgsql.org/doc/basic-usage.html#parameters
-        //    var cmd = new NpgsqlCommand("INSERT INTO posts (id, name, description) VALUES ($1, $2, $3)", await conn)
-        //    {
-        //        Parameters =
-        //        {
-        //            new NpgsqlParameter() { Value = id }, 
-        //            new NpgsqlParameter() { Value = name}, 
-        //            new NpgsqlParameter() { Value = desc} 
-        //        }
-        //    };
-
-
-        //    try { await cmd.ExecuteNonQueryAsync(); }
-        //    catch (Exception ex) { Console.WriteLine($"Query execution failed: {ex.Message}"); }
-        //}
-
-
-
-        //public async void ShowAllPosts()
-        //{
-        //    var cmd = new NpgsqlCommand("SELECT * FROM posts", await conn);
-        //    var reader = await cmd.ExecuteReaderAsync();
-
-        //    var posts = new ObservableCollection<Post>();
-
-        //    while (await reader.ReadAsync())
-        //    {
-        //        var id = (int)reader["id"];
-        //        var name = (string)reader["name"];
-        //        var desc = (string)reader["description"];
-
-        //        posts.Add(new Post { Id = id, Name = name, Description = desc });
-        //    }
-
-        //    postListView.ItemsSource = posts;
-        //}
-
-        ///// <summary>
-        ///// Method that returns the host address depending on the device platform
-        ///// </summary>
-        ///// <returns>Host address</returns>
-        //public static string GetHost()
-        //{
-        //    var platform = Device.RuntimePlatform;
-
-        //    if (platform == Device.Android) return "10.0.2.2";
-
-        //    return "localhost";
-        //}
+        private async void GoToPostCreator(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new CreatorPage());
+        }
 
     }
 }
